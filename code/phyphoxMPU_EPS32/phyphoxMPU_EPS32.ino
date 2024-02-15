0287ff5b9f730a5ad6263f164cd8cd8090783236
Tim Schumann <66968256+T-N-S@users.noreply.github.com>
2024-02-15 03:10:55 +0100

    Added time and millis to export

--- code/phyphoxMPU_EPS32/phyphoxMPU_EPS32.ino
+++ code/phyphoxMPU_EPS32/phyphoxMPU_EPS32.ino
@@ -1,22 +1,42 @@
+/* ============================================
+
+    Sketch for NaWiSchool
+    Output to phyphox app 
+    [This code outputs 3 graphs from the MPU6050 to the phyphox app]
+    Author: T. Schumann
+    Date: 2024-01-20
+
+    Dependencies:
+    phyphox Arduino BLE - https://github.com/phyphox/phyphox-arduino
+    Adafruit_Sensor - https://github.com/adafruit/Adafruit_Sensor
+    MPU6050 - https://github.com/electroniccats/mpu6050
+
+    All rights reserved. Copyright Tim Schumann 2024
+    
+  ===============================================
+*/
+
+
 #include <phyphoxBle.h>
 #include <Wire.h>
 #include <MPU6050.h>
 #include <Math.h>
 
 
-const float offsetX = -0.4;  //input your offset here
-const float offsetY = 0.28;  //input your offset here
-const float offsetZ = -0.75;  //input your offset here
+const float offsetX = 0.0;  //input your offset here
+const float offsetY = 0.0;  //input your offset here
+const float offsetZ = 0.0;  //input your offset here
 
 float frequency = 50;
 int period = (int)1000000 / frequency;
+unsigned long recent;
 
 void receivedData();
 
 // Create a BME280 object
 MPU6050 mpu;
 int16_t ax, ay, az;
-float fx, fy, fz, fi;
+float fx, fy, fz, cpuTime;
 
 void setup() {
   Serial.begin(115200);
@@ -55,8 +75,8 @@ void setup() {
      Channel 0 means a timestamp is created after the BLE package arrives in phyphox
      Channel 1 to N corresponding to the N-parameter which is written in server.write()
   */
-
   firstGraph.setChannel(0, 1);
+
 
   //Second Graph
   PhyphoxBleExperiment::Graph secondGraph;  //Create graph which will plot random numbers over time
@@ -73,7 +93,6 @@ void setup() {
      Channel 0 means a timestamp is created after the BLE package arrives in phyphox
      Channel 1 to N corresponding to the N-parameter which is written in server.write()
   */
-
   secondGraph.setChannel(0, 2);
 
 
@@ -92,96 +111,103 @@ void setup() {
      Channel 0 means a timestamp is created after the BLE package arrives in phyphox
      Channel 1 to N corresponding to the N-parameter which is written in server.write()
   */
-
   thirdGraph.setChannel(0, 3);
 
+
   //Info
-  PhyphoxBleExperiment::InfoField myInfo;  //Creates an info-box.
-  myInfo.setInfo("Hier kann die Frequenz der Messung angepasst werden.");
-  //myInfo.setColor("404040");  //Sets font color. Uses a 6 digit hexadecimal value in "quotation marks".
-  myInfo.setXMLAttribute("size=\"1.2\"");
+  PhyphoxBleExperiment::InfoField info;  //Creates an info-box.
+  info.setInfo("Zeit und Frequenz der Messung.");
+  //info.setColor("404040");  //Sets font color. Uses a 6 digit hexadecimal value in "quotation marks".
+  info.setXMLAttribute("size=\"1.2\"");
 
   //Separator
-  PhyphoxBleExperiment::Separator mySeparator;  //Creates a line to separate elements.
-  mySeparator.setHeight(0.3);                   //Sets height of the separator.
-  mySeparator.setColor("404040");               //Sets color of the separator. Uses a 6 digit hexadecimal value in "quotation marks".
+  PhyphoxBleExperiment::Separator separator;  //Creates a line to separate elements.
+  separator.setHeight(0.5);                   //Sets height of the separator.
+  separator.setColor("404040");               //Sets color of the separator. Uses a 6 digit hexadecimal value in "quotation marks".
 
-  /*//Value
-  PhyphoxBleExperiment::Value myValue;  //Creates a value-box.
-  myValue.setLabel("Number");           //Sets the label
-  myValue.setPrecision(2);              //The amount of digits shown after the decimal point.
-  myValue.setUnit("u");                 //The physical unit associated with the displayed value.
-  myValue.setColor("FFFFFF");           //Sets font color. Uses a 6 digit hexadecimal value in "quotation marks".
-  myValue.setChannel(3);
-  myValue.setXMLAttribute("size=\"2\"");
-  */
+  //Value
+  PhyphoxBleExperiment::Value dispTime;  //Creates a value-box.
+  dispTime.setLabel("Time:");            //Sets the label
+  dispTime.setPrecision(4);              //The amount of digits shown after the decimal point.
+  dispTime.setUnit("s");                 //The physical unit associated with the displayed value.
+  dispTime.setColor("FFFFFF");           //Sets font color. Uses a 6 digit hexadecimal value in "quotation marks".
+  dispTime.setChannel(0);
+  dispTime.setXMLAttribute("size=\"2\"");
+
 
   //Edit
-  PhyphoxBleExperiment::Edit myEdit;
-  myEdit.setLabel("Frequen:");
-  myEdit.setUnit("Hz");
-  myEdit.setSigned(false);
-  myEdit.setDecimal(true);
-  myEdit.setChannel(1);
-  myEdit.setXMLAttribute("min=\"0.01\"");
-  myEdit.setXMLAttribute("max=\"100\"");
+  PhyphoxBleExperiment::Edit freq;
+  freq.setLabel("Frequency:");
+  freq.setUnit("Hz");
+  freq.setSigned(false);
+  freq.setDecimal(true);
+  freq.setChannel(4);
+  freq.setXMLAttribute("min=\"0.01\"");
+  freq.setXMLAttribute("max=\"50\"");
 
   //Export
-  PhyphoxBleExperiment::ExportSet mySet;  //Provides exporting the data to excel etc.
-  mySet.setLabel("mySet");
+  PhyphoxBleExperiment::ExportSet exp;  //Provides exporting the data to excel etc.
+  exp.setLabel("export MPU6050");
 
-  PhyphoxBleExperiment::ExportData myData1;
-  myData1.setLabel("acceleration x");
-  myData1.setDatachannel(1);
+  PhyphoxBleExperiment::ExportData data0;
+  data0.setLabel("time");
+  data0.setDatachannel(0);
 
-  PhyphoxBleExperiment::ExportData myData2;
-  myData2.setLabel("acceleration y");
-  myData2.setDatachannel(2);
+  PhyphoxBleExperiment::ExportData data1;
+  data1.setLabel("acceleration x");
+  data1.setDatachannel(1);
 
-  PhyphoxBleExperiment::ExportData myData3;
-  myData3.setLabel("acceleration z");
-  myData3.setDatachannel(3);
+  PhyphoxBleExperiment::ExportData data2;
+  data2.setLabel("acceleration y");
+  data2.setDatachannel(2);
+
+  PhyphoxBleExperiment::ExportData data3;
+  data3.setLabel("acceleration z");
+  data3.setDatachannel(3);
+
+  PhyphoxBleExperiment::ExportData data4;
+  data4.setLabel("cpu time millis");
+  data4.setDatachannel(4);
 
   //attach to experiment
-  firstView.addElement(firstGraph);    //attach graph to view
-  firstView.addElement(secondGraph);   //attach second graph to view
-  firstView.addElement(thirdGraph);    //attach third graph to view
-  secondView.addElement(myInfo);       //attach info to view
-  secondView.addElement(mySeparator);  //attach separator to view
-  //secondView.addElement(myValue);       //attach value to view
-  secondView.addElement(myEdit);         //attach editfield to view (Linked to value)
-  mpu6050Experiment.addView(firstView);  //attach view to experiment
-  mpu6050Experiment.addView(secondView);
-  mySet.addElement(myData1);                     //attach data to exportSet
-  mySet.addElement(myData2);                     //attach data to exportSet
-  mySet.addElement(myData3);                     //attach data to exportSet
-  mpu6050Experiment.addExportSet(mySet);         //attach exportSet to experiment
+  firstView.addElement(firstGraph);              //attach graph to view
+  firstView.addElement(secondGraph);             //attach second graph to view
+  firstView.addElement(thirdGraph);              //attach third graph to view
+  secondView.addElement(info);                   //attach info to view
+  secondView.addElement(separator);              //attach separator to view
+  secondView.addElement(dispTime);               //attach value to view
+  secondView.addElement(freq);                   //attach editfield to view (Linked to value)
+  mpu6050Experiment.addView(firstView);          //attach view to experiment
+  mpu6050Experiment.addView(secondView);         //attach view to experiment
+  exp.addElement(data0);                         //attach data to exportSet
+  exp.addElement(data1);                         //attach data to exportSet
+  exp.addElement(data2);                         //attach data to exportSet
+  exp.addElement(data3);                         //attach data to exportSet
+  exp.addElement(data4);                         //attach data to exportSet
+  mpu6050Experiment.addExportSet(exp);           //attach exportSet to experiment
   PhyphoxBLE::addExperiment(mpu6050Experiment);  //attach experiment to server
 
-  Wire.begin();
+  Wire.begin(26, 27);
   mpu.initialize();
 }
 
 
 void loop() {
-  unsigned long start_time = micros();  // record start time,
 
-  mpu.getAcceleration(&ax, &ay, &az);
-  fx = (ax * 9.81 / 16384) + offsetX;
-  fy = (ay * 9.81 / 16384) + offsetY;
-  fz = -((az * 9.81 / 16384) - offsetZ);
-  //Serial.println(fx + fy + fz);
+  if (micros() - recent > period) {
 
-  PhyphoxBLE::write(fx, fy, fz);
+    mpu.getAcceleration(&ax, &ay, &az);
+    fx = (ax * 9.81 / 16384) + offsetX;
+    fy = (ay * 9.81 / 16384) + offsetY;
+    fz = (az * 9.81 / 16384) + offsetZ;
+    //Serial.println(fx + fy + fz);
+    cpuTime = millis();
 
+    PhyphoxBLE::write(fx, fy, fz, cpuTime);
+    PhyphoxBLE::poll();  //Only required for the Arduino Nano 33 IoT, but it does no harm for other boards.
 
-  unsigned long execution_time = micros() - start_time;  // calculate execution time
-  unsigned long delay_time = period - execution_time;    // calculate delay time
-  if (delay_time > 0) {
-    delayMicroseconds(delay_time);  // delay for the remaining time
+    recent += period;
   }
-  //Serial.println(delay_time);
-  PhyphoxBLE::poll();  //Only required for the Arduino Nano 33 IoT, but it does no harm for other boards.
 }
 
 void receivedData() {  // get data from PhyPhox appfloat receivedInterval;
@@ -189,7 +215,11 @@ void receivedData() {  // get data from PhyPhox appfloat receivedInterval;
   PhyphoxBLE::read(receivedInterval);
   if (receivedInterval > 0) {
     frequency = receivedInterval;
-    period = (int)1000000 / frequency;
+    period = 1000000 / frequency;
   }
-  //Serial.println(frequency);
+  //Serial.println(period);
 }
+
+
+
+//-------------------------------------
